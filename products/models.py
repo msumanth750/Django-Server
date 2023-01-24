d6a256ba182ac708f5e58833fd64063f73f69150
Sumanth <sumanth@starlly.in>
2023-01-24 21:05:24 +0530

    app products added

---
+++ products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from brands.models import (Brand,
+                            )
+
+# Create your models here.
+class ProductCategory(models.Model):
+    name =models.CharField(max_length=100,unique=True)
+    code = models.CharField(max_length=50,unique=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=150,unique=True)
+    code = models.CharField(max_length=150,unique=True)
+    brand = models.ForeignKey('brands.Brand',on_delete=models.CASCADE)
+    category = models.ForeignKey('ProductCategory',on_delete=models.CASCADE)
