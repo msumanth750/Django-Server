ba0191e94a7a8e30796288dab2a439b35e0d3d42
Sumanth <sumanth@starlly.in>
2023-01-28 10:51:59 +0530

    import export package remove

--- brands/admin.py
+++ brands/admin.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
 
 from .models import (BrandCategory,
                      Brand,
                      )
 
 # Register your models here.
-# admin.site.register(BrandCategory)
-# admin.site.register(Brand)
-
-@admin.register(Brand)
-class BrandAdmin(ImportExportModelAdmin):
-    pass
-
-@admin.register(BrandCategory)
-class BrandCategoryAdmin(ImportExportModelAdmin):
-    pass
+admin.site.register(BrandCategory)
+admin.site.register(Brand)
--- brands/serializers.py
+++ brands/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
-from .models import Brand 
+from .models import Brand
 
 
 class BrandSerializer(serializers.Serializer):
     class Meta:
         model = Brand
-        fields = ('__all__')
+        fields = ['id','name']
